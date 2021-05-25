da96bffcdf4986713de4fcfb8152aba8e193c48d
Teresa Schardt <tschardt@uni-koblenz.de>
2021-05-25 13:59:04 +0200

    Add collisions

--- tiles/Walls.tsx
+++ tiles/Walls.tsx
@@ -1,4 +1,9 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.5" tiledversion="1.5.0" name="Walls" tilewidth="32" tileheight="32" tilecount="256" columns="16">
+<tileset version="1.4" tiledversion="1.4.3" name="Walls" tilewidth="32" tileheight="32" tilecount="256" columns="16">
  <image source="Walls.png" width="512" height="512"/>
+ <tile id="0">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
 </tileset>
