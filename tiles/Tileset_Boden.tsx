c14883c606fce1e5cea4969f9c7921c1dd7832a1
Teresa Schardt <tschardt@uni-koblenz.de>
2021-06-11 11:55:38 +0200

    Add new tiles and website elements

--- tiles/Tileset_Boden.tsx
+++ tiles/Tileset_Boden.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.5" tiledversion="1.5.0" name="Tileset_Boden" tilewidth="32" tileheight="32" tilecount="16" columns="4">
- <image source="Tileset_Boden.png" width="128" height="128"/>
+ <image source="Tileset_Boden.png" width="128" height="256"/>
 </tileset>
