23342829be6792048fbaab73a006720a050ec78b
Teresa Schardt <tschardt@uni-koblenz.de>
2021-06-14 01:27:47 +0200

    Too much stuff

--- tiles/Tileset_Boden.tsx
+++ tiles/Tileset_Boden.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.5" tiledversion="1.5.0" name="Tileset_Boden" tilewidth="32" tileheight="32" tilecount="16" columns="4">
- <image source="Tileset_Boden.png" width="1024" height="1536"/>
+ <image source="Tileset_Boden.png" width="1024" height="2048"/>
 </tileset>
