673528bb310ab7dab5c907143e22b16fa0a3f6cb
Teresa Schardt <tschardt@uni-koblenz.de>
2021-06-23 17:25:48 +0200

    Add collisions

--- tiles/Tileset_Boden.tsx
+++ tiles/Tileset_Boden.tsx
@@ -1,4 +1,14 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="Tileset_Boden" tilewidth="32" tileheight="32" tilecount="4096" columns="64">
+<tileset version="1.5" tiledversion="1.5.0" name="Tileset_Boden" tilewidth="32" tileheight="32" tilecount="2048" columns="32">
  <image source="Tileset_Boden.png" width="1024" height="2048"/>
+ <tile id="224">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="256">
+  <properties>
+   <property name="collides" type="bool" value="true"/>
+  </properties>
+ </tile>
 </tileset>
